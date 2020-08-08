4052ca780908629b2ba5b20307f34542a3800b83
Tóth Tamás <TOTHTOMI57@hotmail.hu>
2020-08-08 14:12:23 +0200

    Fixed an issue :)

---
+++ src/main/java/old/particles/effects/Effect1.java
@@ -0,0 +1,78 @@
+package old.particles.effects;
+
+import java.util.HashMap;
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import old.particles.ParticleEffect;
+
+public class Effect1 extends ParticleEffect {
+
+    private HashMap<Location, BukkitRunnable> running;
+
+    public Effect1(int id, String name) {
+        super(id, name);
+        running = new HashMap<Location, BukkitRunnable>();
+    }
+
+    @Override
+    public void start(final Location start) {
+
+        float num = 400;
+        float fraction = 0.618033f;//0.501616f;
+        Location loc = start;
+
+        for (int i = 0; i < num; i++) {
+            double distance = Math.pow((i / (num - 1)), 3);
+            double angle = 2 * Math.PI * fraction * i;
+
+            double x = (distance * 20) * Math.cos(angle);
+            double z = (distance * 20) * Math.sin(angle);
+
+            loc = loc.add(x, 0, z);
+            loc.getWorld().spawnParticle(Particle.FLAME, loc, 0, 0, 0, 0);
+        }
+		
+		/*
+		
+		BukkitRunnable r = new BukkitRunnable() {
+			int i = 0;
+			Location loc = start;
+			
+			@Override
+			public void run() {
+				double distance = i / (num - 15);
+				double angle = 2 * Math.PI * fraction * i;
+				
+				double x = distance * Math.cos(angle);
+				double z = distance * Math.sin(angle);
+				
+				loc = loc.add(x, 0, z);
+				loc.getWorld().spawnParticle(Particle.FLAME, loc, 0, 0, 0, 0);
+				
+				if (i > num) this.cancel();
+				i++;
+			}
+		};
+		running.put(start, r);
+		r.runTaskTimer(Main.MAIN, 0L, 1L);
+		*/
+    }
+
+    @Override
+    public void stop(Location loc) {
+        running.get(loc).cancel();
+        running.remove(loc);
+    }
+
+    @Override
+    public void stop() {
+        running.forEach((loc, br) -> {
+            br.cancel();
+        });
+        running.clear();
+    }
+
+}
---
+++ src/main/java/old/vegetationold/parts/Tree.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (C) IceyLeagons(https://iceyleagons.net/) 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package old.vegetationold.parts;
+
+import java.util.Random;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import old.vegetationold.Genes;
+import old.vegetationold.themes.ITheme;
+
+public abstract class Tree implements ITree {
+
+    private static Random rand = new Random();
+    protected Location seed;
+    protected Vector origin;
+    protected ITheme theme;
+    protected World world;
+    protected Random random = Tree.rand;
+    protected Player planter;
+
+    public Tree(Genes gene, Player planter, Location seed) {
+        this.planter = planter;
+        this.origin = seed.toVector();
+        this.seed = seed;
+        this.world = seed.getWorld();
+        this.theme = gene.theme;
+    }
+
+    public boolean hasPlanter() {
+        return planter != null;
+    }
+
+    public Player getPlanter() {
+        return planter;
+    }
+
+}
